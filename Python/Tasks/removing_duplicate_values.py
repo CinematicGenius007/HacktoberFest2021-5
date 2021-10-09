6627047c976d1bc0ef18b367ff078c51af04f846
CinematicGenius007 <75163638+CinematicGenius007@users.noreply.github.com>
2021-10-09 18:50:56 +0530

    Update removing_duplicate_values.py

--- Python/Tasks/removing_duplicate_values.py
+++ Python/Tasks/removing_duplicate_values.py
@@ -5,12 +5,21 @@ values with original order reserved.
 """
 
 def removeDuplicate( lists):
-    newlist=[]
+    i=0
+    n=len(lists)
     seen = set()
-    for item in lists:
-        
+    while i < n:
+        if lists[i] in seen:
+            lists.pop(i)
+            n-=1
+        else:
+            seen.add(lists[i])
+            i+=1
+            
+    for j in range(0,n//2):
+        lists[j], lists[n-j-1] = lists[n-j-1], lists[j]
 
-    return newlist
+    return lists
 
 li=[12,24,35,24,88,120,155,88,120,155]
-print removeDuplicate(li)
+print(removeDuplicate(li))
